bdc3d685f629fbe25553c76ff1193d18be351810
Sanjay George <sanjaygeorge16@gmail.com>
2024-10-05 22:35:37 +0530

    Update About section
    - Adjust font sizes
    
    - remove unused classes
    
    - Add border around
    
    - Remove borders from tags

--- ui/src/components/About.svelte
+++ ui/src/components/About.svelte
@@ -14,39 +14,38 @@
         >
     </p>
     <h1
-        class="title md:mb-5 mt-0 text-3xl font-extrabold
+        class="title md:mb-7 mt-0 text-3xl font-extrabold
         md:text-5xl lg:text-6xl text-slate-500"
     >
         About Me
     </h1>
 
-    <div class="container lg:text-xl">
-        <div class="my-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
-            reprehenderit et laborum, rem, dolore eum quod voluptate
-            exercitationem nobis, nihil esse debitis maxime facere minus sint
-            delectus velit in eos quo officiis explicabo deleniti dignissimos.
-            Eligendi illum libero dolorum cum laboriosam corrupti quidem,
-            reiciendis ea magnam? Nulla, impedit fuga!
+    <div class="container lg:text-xl sm:border sm:rounded-3xl sm:px-16">
+        <div class="my-10 text-slate-400">
+            Experienced Software Engineer with over 5 years of expertise in
+            building scalable, data-driven solutions. Adept at collaborating in
+            agile, cross-functional teams to deliver sustainable and optimized
+            products.
         </div>
         <div>
-            <ol class="relative border-s border-gray-200 dark:border-gray-700">
-                <li class="mb-10 ms-4">
+            <ol class="relative border-s border-slate-200">
+                <li class="mb-6 ml-10 ms-4">
                     <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                        class="absolute w-3 h-3 bg-slate-200 rounded-full mt-1.5 -start-1.5
+                        border border-white"
                     ></div>
                     <time
-                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        class="text-xs font-extralight leading-none text-slate-700"
                         >January 2023</time
                     >
                     <h3
-                        class="text-xl font-semibold text-gray-900 dark:text-white"
+                        class="text-xl font-semibold text-slate-900 dark:text-white"
                     >
                         Started at newboxes GmbH
                     </h3>
                     <p
-                        class="mb-4 text-base font-normal text-gray-500
-                         dark:text-gray-400"
+                        class="mb-1 mt-3 text-xl font-normal text-slate-400
+                         dark:text-slate-400"
                     >
                         Built serverless, event-driven solutions for a logistic
                         client. Engineered data pipelines and optimized data
@@ -62,80 +61,75 @@
                         <SkillTag color="orange" name="Svelte" />
                     </div>
                 </li>
-                <li class="mb-10 ms-4">
+                <li class="mb-6 ml-10 ms-4">
                     <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                        class="absolute w-3 h-3 bg-slate-200 rounded-full mt-1.5 -start-1.5
+                        border border-white"
                     ></div>
                     <time
-                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        class="text-xs font-extralight leading-none text-slate-700"
                         >April 2022</time
                     >
                     <h3
-                        class="text-lg font-semibold text-gray-900 dark:text-white"
+                        class="text-xl font-semibold text-slate-900 dark:text-white"
                     >
                         Moved to Germany and started my Masters
                     </h3>
-                    <p
-                        class="text-base font-normal text-gray-500 dark:text-gray-400"
-                    >
+                    <p class="text-xl font-normal text-slate-400">
                         Started my Masters in Applied Computer Science at
                         Hochschule Fulda.
                     </p>
                 </li>
-                <li class="mb-10 ms-4">
+                <li class="mb-6 ml-10 ms-4">
                     <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                        class="absolute w-3 h-3 bg-slate-200 rounded-full mt-1.5 -start-1.5 border border-white"
                     ></div>
                     <time
-                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        class="text-xs font-extralight leading-none text-slate-700"
                         >October 2021</time
                     >
                     <h3
-                        class="text-lg font-semibold text-gray-900 dark:text-white"
+                        class="text-xl font-semibold text-slate-900 dark:text-white"
                     >
                         Team Lead (Bikes division)
                     </h3>
                     <p
-                        class="text-base font-normal text-gray-500 dark:text-gray-400"
+                        class="text-xl font-normal text-slate-400 dark:text-slate-400"
                     >
                         Started leading both teams of the bikes division.
                     </p>
                 </li>
-                <li class="mb-10 ms-4">
+                <li class="mb-6 ml-10 ms-4">
                     <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                        class="absolute w-3 h-3 bg-slate-200 rounded-full mt-1.5 -start-1.5 border border-white"
                     ></div>
                     <time
-                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        class="text-xs font-extralight leading-none text-slate-700"
                         >March 2021</time
                     >
-                    <h3
-                        class="text-lg font-semibold text-gray-900 dark:text-white"
-                    >
-                        Promoted to Team Lead (Used Cars Team, CarTrade)
+                    <h3 class="text-xl font-semibold text-slate-900">
+                        Team Lead (Used Cars Team, CarTrade)
                     </h3>
-                    <p
-                        class="text-base font-normal text-gray-500 dark:text-gray-400"
-                    >
+                    <p class="text-xl font-normal text-slate-400">
                         Led the launch of a new product line for used cars at
                         CarTrade Tech.
                     </p>
                 </li>
-                <li class="mb-10 ms-4">
+                <li class="mb-10 ml-10 ms-4">
                     <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+                        class="absolute w-3 h-3 bg-slate-200 rounded-full mt-1.5 -start-1.5 border border-white"
                     ></div>
                     <time
-                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+                        class="text-xs font-extralight leading-none text-slate-700"
                         >June 2018</time
                     >
                     <h3
-                        class="text-xl font-semibold text-gray-900 dark:text-white"
+                        class="text-xl font-semibold text-slate-900 dark:text-white"
                     >
                         Started at CarTrade Tech
                     </h3>
                     <p
-                        class="text-base mb-4 font-normal text-gray-500 dark:text-gray-400"
+                        class="text-xl mt-3 mb-1 font-normal text-slate-400 dark:text-slate-400"
                     >
                         Started working as SDE1 at CarTrade Tech in the bikes
                         monetization team.
@@ -152,43 +146,5 @@
         </div>
     </div>
 
-    <!-- <div class="section-container pt-6 items-center h-fit gap-8">
-        <div class="section__pic-container w-full">
-            <img src={ProfilePic} alt="Profile picture" class="about-pic" />
-        </div>
-        <div class="about-details-container">
-            <div class="about-containers">
-                <div class="details-container">
-                    <img
-                        src={ExperienceIcon}
-                        alt="Experience icon"
-                        class="icon"
-                    />
-                    <h3>Experience</h3>
-                    <p>2+ years <br />Frontend Development</p>
-                </div>
-                <div class="details-container">
-                    <img
-                        src={EducationIcon}
-                        alt="Education icon"
-                        class="icon"
-                    />
-                    <h3>Education</h3>
-                    <p>B.Sc. Bachelors Degree<br />M.Sc. Masters Degree</p>
-                </div>
-            </div>
-            <div class="text-container">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-                    quis reprehenderit et laborum, rem, dolore eum quod
-                    voluptate exercitationem nobis, nihil esse debitis maxime
-                    facere minus sint delectus velit in eos quo officiis
-                    explicabo deleniti dignissimos. Eligendi illum libero
-                    dolorum cum laboriosam corrupti quidem, reiciendis ea
-                    magnam? Nulla, impedit fuga!
-                </p>
-            </div>
-        </div>
-    </div> -->
     <ArrowIcon scrollTo="#experience" />
 </section>
